5aae7de24726b65c76392c36062b72634ce3e3ba
kangheesoo <audentia16@naver.com>
2020-03-03 18:11:32 +0900

    수정중 메인

--- src/main/java/com/shopping/MVC_reshop/MainController.java
+++ src/main/java/com/shopping/MVC_reshop/MainController.java
@@ -16,30 +16,6 @@ import com.shopping.MVC_reshop.user.UserVO;
  */
 @Controller
 public class MainController { //메인 페이지 컨트롤러
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping("/wiz_want.do")
-	public String main(UserVO vo, UserDAO userdao, HttpSession session) {
-		
-		System.out.println("메인페이지로 이동");
-		
-		
-		//메인 페이지 이동 시 현재 로그인 상태 체크 하여 로그인 상태 인 경우 회원가입 버튼 출력 x
-//		UserVO user  = userdao.getUser(vo);
-//		
-//		if(user != null) {
-//			session.setAttribute("userName", user.getName());
-//			System.out.println("로그인성공");
-//			return "/getBoardList.do";
-//		}else {
-//			System.out.println("로그인실패");
-//			return "login.jsp";
-//		}
-		
-		
-		return "main/wiz_want_main";
-	}
+
 	
 }
