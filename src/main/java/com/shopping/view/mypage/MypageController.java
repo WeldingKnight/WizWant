610e10b97642dfac645851df705fffaf605b8572
ArtHorizon <48702686+ArtHorizon@users.noreply.github.com>
2020-03-04 15:55:36 +0900

    mypage 연결 완

--- src/main/java/com/shopping/view/mypage/MypageController.java
+++ src/main/java/com/shopping/view/mypage/MypageController.java
@@ -2,15 +2,36 @@ package com.shopping.view.mypage;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class MypageController {
-	
-	
+
 	@RequestMapping("/mypage.do")
 	public String mypage() {
-		
+
 		System.out.println("마이페이지로 이동");
 		return "/views/mypage/mypage_main.jsp";
+	}
+
+	@RequestMapping("/update.do")
+	public String update() {
+
+		System.out.println("개인정보수정 페이지로 이동");
+		return "/views/mypage/update.jsp";
+	}
+
+	@RequestMapping("/bookmark.do")
+	public String bookmark() {
+
+		System.out.println("찜목록 페이지로 이동");
+		return "/views/mypage/bookmark.jsp";
+	}
+
+	@RequestMapping("/orders.do")
+	public String orders() {
+
+		System.out.println("주문내역 페이지로 이동");
+		return "/views/mypage/orders.jsp";
 	}
 
 }
