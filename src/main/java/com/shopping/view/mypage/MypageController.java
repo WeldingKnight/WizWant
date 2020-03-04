14935e8cb00943d552e6f255af1c931c9538fede
ArtHorizon <48702686+ArtHorizon@users.noreply.github.com>
2020-03-04 15:36:54 +0900

    mypage 수정

--- src/main/java/com/shopping/view/mypage/MypageController.java
+++ src/main/java/com/shopping/view/mypage/MypageController.java
@@ -1,5 +1,13 @@
 package com.shopping.view.mypage;
 
-public class MypageController {
+import org.springframework.web.bind.annotation.RequestMapping;
 
+public class MypageController {
+	
+	@RequestMapping("/mypage.do")
+	public String mypage() {
+		
+		System.out.println("마이페이지로 이동");
+		return "/views/mypage/mypage_main.jsp";
+	}
 }
