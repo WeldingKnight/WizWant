d7821b090e9774898d4ae8fdc006e706f1c1be84
kangheesoo <audentia16@naver.com>
2020-03-06 17:34:09 +0900

    mainjquery 메인에서만동작하도록 수정

--- src/main/webapp/js/main_page.js
+++ src/main/webapp/js/main_page.js
@@ -1,29 +1,35 @@
 
+$(document).ready(function() {
 
-$(document).ready(function () {
+	/* 메인 페이지 슬라이드 jquery */
+
+	var mainurl = window.location.pathname;
 	
-	/*메인 페이지 슬라이드 jquery*/
-	if (window.location.pathname == '/wiz_want.do') {
-    var slideIndex = 0;
-    showSlides();
+	alert(mainurl);
 
-    function showSlides() {
-        var i;
-        var slides = document.getElementsByClassName("mainSlides");
-        var dots = document.getElementsByClassName("main_dot");
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slideIndex++;
-        if (slideIndex > slides.length) { 
-            slideIndex = 1 
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
-        setTimeout(showSlides, 2000); // Change image every 2 seconds
-    }
-	}z
+	if (mainurl == '/MVC_reshop/wiz_want.do'|| mainurl == '/MVC_reshop/') {
+
+		var slideIndex = 0;
+		showSlides();
+
+		function showSlides() {
+			var i;
+			var slides = document.getElementsByClassName("mainSlides");
+			var dots = document.getElementsByClassName("main_dot");
+			for (i = 0; i < slides.length; i++) {
+				slides[i].style.display = "none";
+			}
+			slideIndex++;
+			if (slideIndex > slides.length) {
+				slideIndex = 1
+			}
+			for (i = 0; i < dots.length; i++) {
+				dots[i].className = dots[i].className.replace(" active", "");
+			}
+			slides[slideIndex - 1].style.display = "block";
+			dots[slideIndex - 1].className += " active";
+			setTimeout(showSlides, 2000); // Change image every 2 seconds
+		}
+	}
+
 });
