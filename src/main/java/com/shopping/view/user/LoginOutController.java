0624de989e4b69250ccd7a708a27629182b6e827
kangheesoo <audentia16@naver.com>
2020-03-10 16:16:35 +0900

    로그인페이지수정

--- src/main/java/com/shopping/view/user/LoginOutController.java
+++ src/main/java/com/shopping/view/user/LoginOutController.java
@@ -1,5 +1,11 @@
 package com.shopping.view.user;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
@@ -7,12 +13,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.support.RequestContext;
 
 //import com.company.AnnotationMVC.user.UserDAO;
 //import com.company.AnnotationMVC.user.UserVO;
 import com.shopping.MVC_reshop.HomeController;
 import com.shopping.MVC_reshop.user.UserDAO;
 import com.shopping.MVC_reshop.user.UserVO;
+
 
 @Controller
 public class LoginOutController {
@@ -26,19 +34,23 @@ public class LoginOutController {
 		return "/views/login&insert/login.jsp";
 	}
 
+
 	@RequestMapping(value ="/login.do",method = RequestMethod.POST)
 	public String login(UserVO vo, UserDAO userdao, HttpSession session) {
-
+		
 		// session: 내가 클라이언트에서 접속하고 있는 동안 개인한테 제공하는 메모리공간이다.(예수님 가라사대)
 		UserVO user = userdao.getUser(vo);
 
 		if (user != null) {
+			
 			session.setAttribute("userId", user.getId());
 			System.out.println("로그인성공");
-			return "wiz_want.do";
+			
+			return "/wiz_want.do";
 		} else {
 			System.out.println("로그인실패");
 			return "/views/login&insert/login.jsp";
+		
 		}
 	}
 
